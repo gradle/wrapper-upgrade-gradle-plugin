f671d196bb3a5a6ee655bd95523a29ffe8061e28
Etienne Studer <etienne@gradle.com>
2022-02-22 14:39:28 +0100

    Polish

--- src/main/java/com/gradle/upgrade/wrapper/UpgradeWrapperPlugin.java
+++ src/main/java/com/gradle/upgrade/wrapper/UpgradeWrapperPlugin.java
@@ -16,8 +16,7 @@ public class UpgradeWrapperPlugin implements Plugin<Project> {
 
         upgradeContainer.configureEach(upgrade -> {
             var taskNameSuffix = upgrade.name.substring(0, 1).toUpperCase() + upgrade.name.substring(1);
-            var upgradeTask = project.getTasks().register("upgradeWrapper" + taskNameSuffix,
-                UpgradeWrapper.class, upgrade);
+            var upgradeTask = project.getTasks().register("upgradeWrapper" + taskNameSuffix, UpgradeWrapper.class, upgrade);
             upgradeAllTask.configure(task -> task.dependsOn(upgradeTask));
         });
     }
