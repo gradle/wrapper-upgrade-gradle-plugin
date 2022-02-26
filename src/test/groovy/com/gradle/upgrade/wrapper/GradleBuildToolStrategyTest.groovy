b38a72942ecda01e2738fe6d8d7bd8abc74edd1a
Etienne Studer <etienne@gradle.com>
2022-02-26 14:57:38 +0100

    Implement lookupLatestVersion and runWrapper for MavenBuildToolStrategy

--- src/test/groovy/com/gradle/upgrade/wrapper/GradleBuildToolStrategyTest.groovy
+++ src/test/groovy/com/gradle/upgrade/wrapper/GradleBuildToolStrategyTest.groovy
@@ -1,60 +1,64 @@
 package com.gradle.upgrade.wrapper
 
+import spock.lang.Shared
 import spock.lang.Specification
 import spock.lang.TempDir
 
 import java.nio.file.Files
 import java.nio.file.Path
 
-class GradleUtilsTest extends Specification {
+class GradleBuildToolStrategyTest extends Specification {
+
+    @Shared
+    BuildToolStrategy gradleBuildToolStrategy = BuildToolStrategy.GRADLE
 
     @TempDir
     Path workingDir
 
-    def "extract current gradle version bin"() {
+    def "extract current Gradle version bin"() {
         given:
         createGradleWrapperProperties().text = standard('7.3.3', 'bin')
 
         when:
-        def version = GradleUtils.extractCurrentGradleVersion(workingDir)
+        def version = gradleBuildToolStrategy.extractCurrentVersion(workingDir)
 
         then:
         version == '7.3.3'
     }
 
-    def "extract current gradle version all"() {
+    def "extract current Gradle version all"() {
         given:
         createGradleWrapperProperties().text = standard('7.2', 'all')
 
         when:
-        def version = GradleUtils.extractCurrentGradleVersion(workingDir)
+        def version = gradleBuildToolStrategy.extractCurrentVersion(workingDir)
 
         then:
         version == '7.2'
     }
 
-    def "extract current gradle distributionUrl not found"() {
+    def "extract current Gradle distributionUrl not found"() {
         given:
         createGradleWrapperProperties().text = 'unexpected'
 
         when:
-        GradleUtils.extractCurrentGradleVersion(workingDir)
+        gradleBuildToolStrategy.extractCurrentVersion(workingDir)
 
         then:
         def e = thrown(IllegalStateException)
         e.message == "Could not find property 'distributionUrl' in file gradle/wrapper/gradle-wrapper.properties"
     }
 
-    def "extract current gradle version unknown"() {
+    def "extract current Gradle version unknown"() {
         given:
         createGradleWrapperProperties().text = 'distributionUrl=unknown'
 
         when:
-        GradleUtils.extractCurrentGradleVersion(workingDir)
+        gradleBuildToolStrategy.extractCurrentVersion(workingDir)
 
         then:
         def e = thrown(IllegalStateException)
-        e.message == "Could not extract Gradle version from property 'distributionUrl': unknown"
+        e.message == "Could not extract version from property 'distributionUrl': unknown"
     }
 
     private static String standard(String gradleVersion, String gradleDistro) {
